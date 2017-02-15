32f42b869a88c428f8bb1238e9ff15f1b57444d7
Taner Yavuz <taner.yavuz@cybercom.com>
2017-02-15 09:02:34 +0100

    Added receive transmission function

--- Arduino/LoRaChallenge/LoRaChallenge.ino
+++ Arduino/LoRaChallenge/LoRaChallenge.ino
@@ -9,46 +9,27 @@
 #define debugSerial SerialUSB
 #define loraSerial Serial2
 
-//const uint8_t devAddr[4] =
-//{
-//  0xED, 0x0D, 0xDA, 0xDB,
-//};
-//
-//const uint8_t appSKey[16] =
-//{
-//  0x6B, 0x2C, 0x4B, 0xF0,
-//  0x59, 0xFB, 0x2E, 0xCC,
-//  0x63, 0x37, 0x63, 0xF0,
-//  0x2A, 0xC9, 0xD3, 0x05,
-//};
-//
-//const uint8_t nwkSKey[16] =
-//{
-//  0x46, 0x90, 0x9F, 0x22,
-//  0x34, 0x38, 0xF8, 0xA9,
-//  0x02, 0x25, 0x21, 0x1A,
-//  0xD1, 0xDE, 0xF5, 0x36,
-//};
-
-
 const uint8_t testPayload[] =
 {
-  0x13, 0x37, 0x13, 0x37
+  0x13, 0x37,
 };
 
-void RED() {
+void RED()
+{
   digitalWrite(LED_RED, LOW);
   digitalWrite(LED_GREEN, HIGH);
   digitalWrite(LED_BLUE, HIGH);
 }
 
-void GREEN() {
+void GREEN()
+{
   digitalWrite(LED_RED, HIGH);
   digitalWrite(LED_GREEN, LOW);
   digitalWrite(LED_BLUE, HIGH);
 }
 
-void BLUE() {
+void BLUE()
+{
   digitalWrite(LED_RED, HIGH);
   digitalWrite(LED_GREEN, HIGH);
   digitalWrite(LED_BLUE, LOW);
@@ -115,50 +96,61 @@ void LoRaSetup()
 
 void loop()
 {
-   String reading = getTemperature();
-
-    switch (LoRaBee.send(1, (uint8_t*)reading.c_str(), reading.length()))
-    {
-    case NoError:
-      debugSerial.println("Successful transmission.");
-      GREEN();
-      break;
-    case NoResponse:
-      debugSerial.println("There was no response from the device.");
-      break;
-    case Timeout:
-      debugSerial.println("Connection timed-out. Check your serial connection to the device! Sleeping for 20sec.");
-      delay(20000);
-      break;
-    case PayloadSizeError:
-      debugSerial.println("The size of the payload is greater than allowed. Transmission failed!");
-      break;
-    case InternalError:
-      debugSerial.println("Oh No! This shouldn't happen. Something is really wrong! The program will reset the RN module.");
-      LoRaSetup();
-      break;
-    case Busy:
-      debugSerial.println("The device is busy. Sleeping for 10 extra seconds.");
-      delay(10000);
-      break;
-    case NetworkFatalError:
-      debugSerial.println("There is a non-recoverable error with the network connection. The program will reset the RN module.");
-      LoRaSetup();
-      break;
-    case NotConnected:
-      debugSerial.println("The device is not connected to the network. The program will reset the RN module.");
-      LoRaSetup();
-      break;
-    case NoAcknowledgment:
-      debugSerial.println("There was no acknowledgment sent back!");
-      break;
-    default:
-      break;
-    }
-    // Delay between readings
-    // 60 000 = 1 minute
-    delay(5000); 
-    ledOFF();
+  String reading = getTemperature();
+  switch (LoRaBee.send(1, (uint8_t*)reading.c_str(), reading.length())) //switch (LoRaBee.send(1, testPayload, 2))
+  {
+  case NoError:
+    debugSerial.println("Successful transmission.");
+    GREEN();
+    break;
+  case NoResponse:
+    debugSerial.println("There was no response from the device.");
+    RED();
+    LoRaSetup();
+    break;
+  case Timeout:
+    debugSerial.println("Connection timed-out. Check your serial connection to the device! Sleeping for 20sec.");
+    RED();
+    delay(20000);
+    break;
+  case PayloadSizeError:
+    debugSerial.println("The size of the payload is greater than allowed. Transmission failed!");
+    RED();
+    break;
+  case InternalError:
+    debugSerial.println("Oh No! This shouldn't happen. Something is really wrong! The program will reset the RN module.");
+    RED();
+    LoRaSetup();
+    break;
+  case Busy:
+    debugSerial.println("The device is busy. Sleeping for 10 extra seconds.");
+    RED();
+    delay(10000);
+    break;
+  case NetworkFatalError:
+    debugSerial.println("There is a non-recoverable error with the network connection. The program will reset the RN module.");
+    RED();
+    LoRaSetup();
+    break;
+  case NotConnected:
+    debugSerial.println("The device is not connected to the network. The program will reset the RN module.");
+    RED();
+    LoRaSetup();
+    break;
+  case NoAcknowledgment:
+    debugSerial.println("There was no acknowledgment sent back!");
+    RED();
+    break;
+  default:
+    break;
+  }
+  // Delay between readings
+  // 60 000 = 1 minute
+  delay(5000); 
+  
+  receiveData();
+  
+  ledOFF();
 }
 
 String getTemperature()
@@ -169,3 +161,31 @@ String getTemperature()
   
   return String(temp);
 }
+
+
+void receiveData()
+{
+  // After we have send some data, we can receive some data
+  // First we make a buffer
+  uint8_t payload[64];
+  // Now we fill the buffer and
+  // len = the size of the data
+  uint16_t len = LoRaBee.receive(payload, 64);
+  String HEXPayload = "";
+
+  // When there is no payload the lorabee.receive will return 0
+  // I filter this out
+  if (len > 0)
+  {
+    for (int i = 0; i < len; i++)
+    {
+      HEXPayload += String(payload[i], HEX);
+    }
+    debugSerial.println(HEXPayload);
+  }
+  else
+  {
+    debugSerial.println("no received transmission!");
+  }
+}
+
